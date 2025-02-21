6c1a6694b0ec7bea590cd0a0be305da06b5aca43
NeloySaha <neloy.saha456@gmail.com>
2025-02-21 21:25:15 +0600

    experimenting

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const fs = require('fs');
-const http = require('http');
-const url = require('url');
-const slugify = require('slugify');
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
+const slugify = require("slugify");
 
-const replaceTemplate = require('./modules/replaceTemplate');
+const replaceTemplate = require("./modules/replaceTemplate");
 
 ////////////////////////////
 ////////// FILES ///////////
@@ -36,17 +36,17 @@ console.log("File writing done");
 ////////// SERVER //////////
 ////////////////////////////
 
-const data = fs.readFileSync('./dev-data/data.json', 'utf-8');
+const data = fs.readFileSync("./dev-data/data.json", "utf-8");
 const productData = JSON.parse(data);
 const tempOverview = fs.readFileSync(
-    './templates/template-overview.html',
-    'utf-8'
+    "./templates/template-overview.html",
+    "utf-8"
 );
 const tempProduct = fs.readFileSync(
-    './templates/template-product.html',
-    'utf-8'
+    "./templates/template-product.html",
+    "utf-8"
 );
-const tempCard = fs.readFileSync('./templates/template-card.html', 'utf-8');
+const tempCard = fs.readFileSync("./templates/template-card.html", "utf-8");
 
 const slugs = productData.map((product) =>
     slugify(product.productName, { lower: true })
@@ -55,7 +55,7 @@ const slugs = productData.map((product) =>
 const server = http.createServer((req, res) => {
     const { query, pathname } = url.parse(req.url, true);
 
-    if (pathname === '/' || pathname === '/overview') {
+    if (pathname === "/" || pathname === "/overview") {
         // OVERVIEW
         const cardsHtml = productData.map((el) =>
             replaceTemplate(tempCard, el)
@@ -63,17 +63,17 @@ const server = http.createServer((req, res) => {
 
         const overviewHtml = tempOverview.replace(
             /{%PRODUCT_CARDS%}/g,
-            cardsHtml.join('')
+            cardsHtml.join("")
         );
 
         res.writeHead(200, {
-            'content-type': 'text/html',
+            "content-type": "text/html",
         });
         res.end(overviewHtml);
-    } else if (pathname === '/product') {
+    } else if (pathname === "/product") {
         // PRODUCT
         res.writeHead(200, {
-            'content-type': 'text/html',
+            "content-type": "text/html",
         });
 
         const requestedProduct = productData.find(
@@ -81,23 +81,23 @@ const server = http.createServer((req, res) => {
         );
         const productHtml = replaceTemplate(tempProduct, requestedProduct);
         res.end(productHtml);
-    } else if (pathname === '/api') {
+    } else if (pathname === "/api") {
         // API
         res.writeHead(200, {
-            'content-type': 'application/json',
+            "content-type": "application/json",
         });
         res.end(productData);
     } else {
         // NOT-FOUND
         res.writeHead(404, {
-            'content-type': 'text/html',
-            'my-header': 'Hello from the header',
+            "content-type": "text/html",
+            "my-header": "Hello from the header",
         });
-        res.end('<h1>Page not found</h1>');
+        res.end("<h1>Page not found</h1>");
     }
 });
 
 // localhost ip address -> 127.0.0.1 --> port address is 7001 here so, localhost:7001 is 127.0.0.1:7001
 server.listen(7001, () => {
-    console.log('Listening to port 7001');
+    console.log("Listening to port 7001");
 });
